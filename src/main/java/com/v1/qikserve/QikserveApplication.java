88f7f11477c8ed13b245c11361ce53714ac6556e
Lupesms97 <lupesms97@gmail.com>
2024-04-24 23:58:05 -0300

    dev: enable feign client

--- src/main/java/com/v1/qikserve/QikserveApplication.java
+++ src/main/java/com/v1/qikserve/QikserveApplication.java
@@ -2,8 +2,10 @@ package com.v1.qikserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class QikserveApplication {
 
 	public static void main(String[] args) {
