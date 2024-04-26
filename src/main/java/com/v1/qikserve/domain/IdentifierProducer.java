26b4a3598a218b67a2c54d72165aee15bf96749c
Lupesms97 <lupesms97@gmail.com>
2024-04-26 12:28:31 -0300

    dev: class to create a unique identifier

--- src/main/java/com/v1/qikserve/domain/IdentifierProducer.java
+++ src/main/java/com/v1/qikserve/domain/IdentifierProducer.java
@@ -2,23 +2,28 @@ package com.v1.qikserve.domain;
 
 
 
+
 import java.util.UUID;
 
 
 public class IdentifierProducer {
 
-    private final String identifier;
+    private final UUID identifier;
 
-    private IdentifierProducer() {
-        this.identifier = generateIdentifier();
+    private IdentifierProducer(UUID identifier) {
+        this.identifier = identifier;
     }
 
     public static IdentifierProducer createInstance() {
-        return new IdentifierProducer();
+        UUID generatedIdentifier = generateIdentifier();
+        return new IdentifierProducer(generatedIdentifier);
     }
 
-    private String generateIdentifier() {
-        return UUID.randomUUID().toString();
+    private static UUID generateIdentifier() {
+        return UUID.randomUUID();
     }
 
+    public UUID getIdentifier() {
+        return identifier;
+    }
 }
