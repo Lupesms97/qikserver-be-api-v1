f1f0880a8e5f419115e0b492eb6ec47e3f5aec62
Lupesms97 <lupesms97@gmail.com>
2024-04-29 21:24:07 -0300

    feat: Promotion class and repository

--- src/main/java/com/v1/qikserve/domain/entity/Promotion.java
+++ src/main/java/com/v1/qikserve/domain/entity/Promotion.java
@@ -1,8 +1,21 @@
 package com.v1.qikserve.domain.entity;
 
 import com.v1.qikserve.domain.entity.promotionDetails.PromotionDetails;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
 
 
 @Getter
@@ -14,9 +27,13 @@ import lombok.*;
 @Entity
 public class Promotion {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String promotion_id;
+    @Column(length = 100)
     private String type;
-    @Embedded
+    @OneToOne(cascade = CascadeType.PERSIST, orphanRemoval = true)
     private PromotionDetails details;
+
 
 }
--- src/main/java/com/v1/qikserve/infrastructure/repositoryImpl/PromotionRepositoryImpl.java
+++ src/main/java/com/v1/qikserve/infrastructure/repositoryImpl/PromotionRepositoryImpl.java
@@ -5,6 +5,8 @@ import com.v1.qikserve.domain.repository.PromotionRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface PromotionRepositoryImpl extends JpaRepository<Promotion, String> , PromotionRepository {
+public interface PromotionRepositoryImpl extends JpaRepository<Promotion, UUID> , PromotionRepository {
 }
